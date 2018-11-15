2d4fc133de69254872f9c400e071c780909d6ba4
webpropopuli <webpropopuli@gmail.com>
2018-11-15 01:04:06 -0500

    Updates

---
+++ precache-manifest.768b0fb634101d19b1074bedf4b662f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c458845f7a3feb23084",
+    "url": "/gomoku-react/static/js/runtime~main.9c458845.js"
+  },
+  {
+    "revision": "859b092e99c2d0a383c8",
+    "url": "/gomoku-react/static/js/main.859b092e.chunk.js"
+  },
+  {
+    "revision": "8649b2a3c146a3d374b2",
+    "url": "/gomoku-react/static/js/1.8649b2a3.chunk.js"
+  },
+  {
+    "revision": "859b092e99c2d0a383c8",
+    "url": "/gomoku-react/static/css/main.92820f10.chunk.css"
+  },
+  {
+    "revision": "909b6e5a382fad8544e8330153ec993a",
+    "url": "/gomoku-react/index.html"
+  }
+];
